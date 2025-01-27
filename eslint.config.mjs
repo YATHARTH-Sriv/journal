d731e08a9a65825ba6c58022df3716e6a3ad7dfb
yatharth srivastava <yatharthsriv18@gmail.com>
2025-01-28 01:36:32 +0530

    linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,20 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-
 const eslintConfig = [
-  // ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    "extends": ["next/core-web-vitals", "next/typescript"],
-    "rules": {
-      "react/no-unescaped-entities": "off",
-      "@next/next/no-page-custom-font": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/no-explicit-any":"off"
-  
-      
-    }
-  }
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
